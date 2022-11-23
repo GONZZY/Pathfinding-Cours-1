c2a6d6397a5fb5560c97c110025d71366fd8d6e8
GONZZY <olivier.goncalves@live.ca>
2022-11-23 11:29:16 -0500

    Bibliothèque Graph Complétée

--- Program.cs
+++ Program.cs
@@ -1,1 +1,11 @@
-﻿
+﻿Graph g1 = new Graph(5);
+
+// 0 --> 1
+g1.AddEdge(0, 1);
+
+// 1 --> 2
+// 2 --> 1
+g1.AddBidirectEdge(1, 2);
+
+
+Console.WriteLine(g1.CreateNeighboursDisplay());
--- graph.cs
+++ graph.cs
@@ -1,4 +1,5 @@
-namespace PathFinding;
+using System.Text;
+
 
 // Un graphe qui a toujours nNodes nombre de noeuds (peut pas changer le nombre de noeuds pendant 
 // l'exécution, mais permet l'ajout de chemin après construction)
@@ -25,5 +26,42 @@ public class Graph
             adjacencyNeighborsLists[i] = new List<int>();
         }
     }
+    // Ajouter UN chemin direct unidirectionnel entre le noeud identifié par IDnodeA et le IDnodeB
+    public void AddEdge(int IDnodeA, int IDnodeB)
+        => adjacencyNeighborsLists[IDnodeA].Add(IDnodeB);
 
+    // Ajouter un chemin bidirectionnel
+    public void AddBidirectEdge(int IDnodeA, int IDnodeB)
+    {
+        AddEdge(IDnodeA, IDnodeB);
+        AddEdge(IDnodeB, IDnodeA);
+    }
+    // ToArray retourne un tableau contenant les éléments de la liste
+    // Ceci fait une copie et ne retourne pas la liste en tant que telle
+    public int[] GetNeighbours(int IDnode) => adjacencyNeighborsLists[IDnode].ToArray();
+
+    // Crée un string des voisins comme suit:
+    //
+    // Node 0: 1,2, ...
+    // Node 1: 3,2, ...
+    // Node 2: 3,5, ...
+    // ...
+    public string CreateNeighboursDisplay()
+    {
+        // La capacité de notre StringBuilder peut être déterminer
+        // Permet de ne pas avoir a faire pleins de copies
+        // Rapidement on met nNodes * 10, mais ceci n'est pas exacte.
+        StringBuilder sb = new StringBuilder(nNodes * 10);
+        for (int i = 0; i < nNodes; ++i)
+        {
+            int[] currentNodeNeighbors = GetNeighbours(i);
+            sb.Append($"Node {i}: ");
+            for (int j = 0; j < currentNodeNeighbors.Length; j++)
+            {
+                sb.Append($"{currentNodeNeighbors[j]} ");
+            }
+            sb.Append("\n");
+        }
+        return sb.ToString();
+    }
 }
